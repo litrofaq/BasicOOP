8da96908134ed9ed68602c748d36fe6803901972
litrofaq <litrofaq@yandex.ru>
2023-04-25 07:32:21 +0500

    Start base project

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BasicOOP;
+namespace BasicOOP;
+
+public class Program
+{
+    static Student[] students;
+
+    static void PrintStudent(Student student)
+    {
+        Console.WriteLine("{0,-15}{1}", student.FirstName, student.LastName);
+    }
+
+    static void Main()
+    {
+        students = new Student[2];
+        students[0] = new Student
+        {
+            FirstName = "John",
+            LastName = "Johnes",
+            Age = 19
+        };
+        students[1] = new Student
+        {
+            FirstName = "William",
+            LastName = "Williams",
+            Age = 18
+        };
+
+        PrintStudent(students[0]);
+        PrintStudent(students[1]); 
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,8 @@
+﻿namespace BasicOOP;
+
+public class Student
+{
+    public int Age;
+    public string FirstName;
+    public string LastName;
+}
